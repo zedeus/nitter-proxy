5c3d754a7ba3093d60c74ed5dbb42e0f8e677db4
Zed <zedeus@pm.me>
2026-02-13 00:56:28 +0100

    Add /pic/ media proxy and /api/ route
    Fixes #2

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"hash/fnv"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/Noooste/azuretls-client"
 )
 
 var session *azuretls.Session
+
+func hash(s string) uint64 {
+	h := fnv.New64a()
+	h.Write([]byte(s))
+	return h.Sum64()
+}
 
 func formatURL(u *url.URL) string {
 	u.Path = strings.TrimLeft(u.Path, "/")
@@ -41,14 +49,25 @@ func newSession() *azuretls.Session {
 	return s
 }
 
-func proxyHandler(w http.ResponseWriter, req *http.Request) {
+func apiProxyHandler(w http.ResponseWriter, req *http.Request) {
 	if session == nil {
 		session = newSession()
 	}
 
+	path, err := url.PathUnescape(req.PathValue("url"))
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(400)
+		io.WriteString(w, "Invalid URL")
+		return
+	}
+
+	url := req.URL
+	url.Path = path
+
 	resp, err := session.Do(&azuretls.Request{
 		Method:     http.MethodGet,
-		Url:        formatURL(req.URL),
+		Url:        formatURL(url),
 		Header:     copyHeaders(req.Header),
 		IgnoreBody: true,
 	})
@@ -77,10 +96,64 @@ func proxyHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func picProxyHandler(w http.ResponseWriter, req *http.Request) {
+	url, err := url.PathUnescape(req.PathValue("url"))
+	if err != nil {
+		w.WriteHeader(400)
+		io.WriteString(w, "Invalid URL")
+		return
+	}
+
+	etag := strconv.FormatUint(hash(url), 10)
+	if req.Header.Get("If-None-Match") == etag {
+		fmt.Println("[PIC] ETag matched:", url)
+		w.WriteHeader(304)
+		return
+	}
+
+	if strings.Contains(req.URL.Path, "orig/") {
+		url = url + "?name=orig"
+	}
+
+	if !strings.Contains(url, "twimg.com") {
+		url = "pbs.twimg.com/" + url
+	}
+
+	if !strings.HasPrefix(url, "https://") {
+		url = "https://" + url
+	}
+
+	resp, err := http.DefaultClient.Get(url)
+	if err != nil {
+		fmt.Println("[PIC] Error:", err)
+		w.WriteHeader(502)
+		io.WriteString(w, "Proxy Error")
+		return
+	}
+
+	defer resp.Body.Close()
+
+	for k, v := range resp.Header {
+		for _, val := range v {
+			w.Header().Add(k, val)
+		}
+	}
+
+	w.Header().Add("ETag", etag)
+
+	w.WriteHeader(resp.StatusCode)
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		fmt.Println("[PIC] Write error:", err)
+	}
+}
+
 func main() {
 	defer session.Close()
 
-	http.HandleFunc("/", proxyHandler)
+	http.HandleFunc("/api/{url...}", apiProxyHandler)
+	http.HandleFunc("/pic/{url}", picProxyHandler)
+	http.HandleFunc("/pic/orig/{url}", picProxyHandler)
 	fmt.Println("Serving at localhost:7000")
 	log.Fatal(http.ListenAndServe("localhost:7000", nil))
 }
